737e4c6fb54fce50238ed43fce5acd5242dddf98
Ale <alerodriguezgarcia@me.com>
2021-08-30 21:53:01 -0600

    Calculate the distance between two coordinates using the Haversine Formula. Calculates the delta between two given variables.

---
+++ blueprints/main/calculate_distance.py
@@ -0,0 +1,14 @@
+import math
+
+def calculate_delta(variable_a, variable_b):
+	"""
+	Calculates the delta between two given variables.
+
+	Args:
+		variable_a (float): Variable a
+		variable_b (float): Variable b
+
+	Returns:
+		variable_b - variable_a (float): The difference between the two given variables.
+	"""
+	return variable_b - variable_a

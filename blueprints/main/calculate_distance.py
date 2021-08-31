8e1d2feb73cd618ade19640ddd5b9bdfe2f42537
Ale <alerodriguezgarcia@me.com>
2021-08-30 21:53:52 -0600

    Calculates the haversine angle of the two given points. The haversine function computes half a versine of the angle θ.

--- blueprints/main/calculate_distance.py
+++ blueprints/main/calculate_distance.py
@@ -12,3 +12,19 @@ def calculate_delta(variable_a, variable_b):
 		variable_b - variable_a (float): The difference between the two given variables.
 	"""
 	return variable_b - variable_a
+
+def calculate_haversine_angle(variable_a, variable_b):
+	"""
+	Calculates the haversine angle of the two given points. The haversine function
+	computes half a versine of the angle θ.
+
+	Args:
+		variable_a (float): Variable a
+		variable_b (float): Variable b
+
+	Returns:
+		haversine_angle (float): The calculated versine angle.  
+	"""
+	haversine_angle = math.pow(math.sin(math.radians(calculate_delta(variable_a=variable_a, variable_b=variable_b))/2),2)
+	
+	return haversine_angle

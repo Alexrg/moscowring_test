2317e984444e81ed3382d77ae0c95bc3e68ab54e
Ale <alerodriguezgarcia@me.com>
2021-08-30 22:00:13 -0600

    Using the haversine formula, calculate the distance in km, between two points. The haversine formula determines the great-circle distance between two points on a sphere given their latitude and longitude.

--- blueprints/main/calculate_distance.py
+++ blueprints/main/calculate_distance.py
@@ -28,3 +28,25 @@ def calculate_haversine_angle(variable_a, variable_b):
 	haversine_angle = math.pow(math.sin(math.radians(calculate_delta(variable_a=variable_a, variable_b=variable_b))/2),2)
 	
 	return haversine_angle
+
+def calculate_distance(latitude_a, longitud_a, latitude_b, longitud_b):
+	"""
+	Using the haversine formula, calculate the distance in km, between two points.
+	The haversine formula determines the great-circle distance between two points
+	on a sphere given their latitude and longitude.
+
+	Args:
+		latitude_a (float): Latitude point of the main location.
+		longitude_a (float): Longitud point of the main location.
+		latitude_b (float): Latitude point of the secondary location.
+		longitude_b (float): Longitud point of the secondary location.
+
+	Returns:
+		distance (float): The distance in kilometers between two points.
+	"""
+	earth_radius = 6371
+	distance = 0
+
+	distance = 2 * earth_radius * math.asin(math.sqrt(calculate_haversine_area(latitude_a, longitud_a, latitude_b, longitud_b)))
+
+	return round(distance, 1)

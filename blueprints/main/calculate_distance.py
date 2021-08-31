6c09104484b3a3322be65fd30640019475cc6335
Ale <alerodriguezgarcia@me.com>
2021-08-31 05:23:05 -0600

    The haversine formula determines the great-circle distance between two points on a sphere given their longitudes and latitudes. The law of haversines, calculates the area of a spherical triangle.

--- blueprints/main/calculate_distance.py
+++ blueprints/main/calculate_distance.py
@@ -29,6 +29,28 @@ def calculate_haversine_angle(variable_a, variable_b):
 	
 	return haversine_angle
 
+def calculate_haversine_area(latitude_a, longitud_a, latitude_b, longitud_b):
+	"""
+	The haversine formula determines the great-circle distance between two points on
+	a sphere given their longitudes and latitudes. The law of haversines, calculates
+	the area of a spherical triangle.
+	
+	Args:
+		latitude_a (float): Latitude point of the main location.
+		longitude_a (float): Longitud point of the main location.
+		latitude_b (float): Latitude point of the secondary location.
+		longitude_b (float): Longitud point of the secondary location.
+
+	Returns:
+		area (float): The area of the spherical triangle
+	"""
+	haversine_longitud = calculate_haversine_angle(variable_a=longitud_a, variable_b=longitud_b)
+	haversine_latitude = calculate_haversine_angle(variable_a=latitude_a, variable_b=latitude_b)
+
+	area = haversine_latitude + math.cos(math.radians(latitude_a)) * math.cos(math.radians(latitude_b)) * haversine_longitud
+	
+	return area
+
 def calculate_distance(latitude_a, longitud_a, latitude_b, longitud_b):
 	"""
 	Using the haversine formula, calculate the distance in km, between two points.
